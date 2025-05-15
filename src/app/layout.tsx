e80b281edfdef733acb2955538ea72f2af90d275
kr-aayushigupta <aayushi.g@knackroot.com>
2025-05-15 15:07:45 +0530

    ZuStand

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased mx-auto max-w-[1200px] bg-green-200 `}
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased mx-auto max-w-[1200px] bg-green-100 `}
       >
         <Navbar/>
         {children}
