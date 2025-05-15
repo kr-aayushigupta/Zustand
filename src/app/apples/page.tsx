112d4c43a62879269d5d8708c2a3cbb1637daceb
kr-aayushigupta <aayushi.g@knackroot.com>
2025-05-15 12:58:38 +0530

    zustand

--- src/app/apples/page.tsx
+++ src/app/apples/page.tsx
@@ -1,18 +1,16 @@
 "use client";
 
-import { usebearStore } from "../store/bearStore";
-import Link from "next/link";
-
-export default function bearCounter() {
-  const bears = usebearStore((state) => state.bears);
-  const increase = usebearStore((state) => state.increase);
-  const reset = usebearStore((state) => state.reset);
-  const decrease=usebearStore((state)=>state.decrease)
+import { useAppleStore } from "../store/AppleStore";
+export default function AppleCounter() {
+  const apples = useAppleStore((state) => state.Apples);
+  const increase = useAppleStore((state) => state.increase);
+  const reset = useAppleStore((state) => state.reset);
+  const decrease=useAppleStore((state)=>state.decrease)
 
   return (
     <div className="mt-10">
          
-      <p className="text-white text-2xl text-center font-semibold"> Teddy Bears  🐻 : {bears}</p>
+      <p className="text-white text-2xl text-center font-semibold">Apples : {apples}</p>
       <div className="flex justify-center gap-4 mt-8">
       <button onClick={increase} className="btn text-white bg-green-600 hover:cursor-pointer hover:bg-green-500 rounded-md px-4 py-2">
         ADD 
